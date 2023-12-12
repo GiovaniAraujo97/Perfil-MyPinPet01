13e7d96b8269e151214191cab23814bd2615dad0
Giovani Araujo <113481569+GiovaniAraujo97@users.noreply.github.com>
2023-12-12 12:29:09 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function openWhatsApp() {
+    window.open('https://wa.me/5511958520419'),('blank');
+}
+
+function openInsta() {
+    window.open('https://www.instagram.com/gi0vanii/'), ('blank')
+}
+
+function openEmail() {
+    window.open('mailto:giovanijesus1997@gmail.com'),('blank')
+}
